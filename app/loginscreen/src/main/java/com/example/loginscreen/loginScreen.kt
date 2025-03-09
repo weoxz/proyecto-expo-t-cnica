df19483258e8d46ce9c101f0c4f56b9ceea81881
davidVargas <davidvargasfms@gmail.com>
2025-03-09 17:52:44 -0600

    segunda parte el boton ya esta funcional y cambia de pantalla para la de crear nuevo usuario.

--- app/loginscreen/src/main/java/com/example/loginscreen/loginScreen.kt
+++ app/loginscreen/src/main/java/com/example/loginscreen/loginScreen.kt
@@ -1,6 +1,8 @@
 package com.example.loginscreen
 
+import android.content.Intent // Importa la clase Intent
 import android.os.Bundle
+import android.widget.Button // Importa la clase Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,21 @@ class loginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_screen)
+        setContentView(R.layout.activity_login_screen) // Asegúrate que este es el layout correcto
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Añade el código para el botón aquí:
+        val nextScreenButton: Button = findViewById(R.id.NextScreen)
+        nextScreenButton.setOnClickListener {
+            val intent = Intent(this,SecondScreen ::class.java)
+            startActivity(intent)
+            //Si quieres que la pantalla loginScreen se cierre al cambiar de activity
+            //agregas la siguiente linea de codigo.
+            //finish()
         }
     }
 }
